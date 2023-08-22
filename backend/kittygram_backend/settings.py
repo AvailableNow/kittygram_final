5761c12ee6355a87a025f4a5c62609025f71be8b
AvailableNow <currently.out.of.service@gmail.com>
2023-08-22 14:47:12 -0400

    fix image downloads

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -105,8 +105,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
